095bc0ddf4e58e22714ca24457b1ad87f2b30aa6
ronaldscruz <ronald.scruz16@gmail.com>
2019-02-07 20:36:09 -0200

    some fixes

---
+++ index_menu.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<a href="#" class="navbar-brand font-weight-bold"><img src="img/logo.png" width="130" height="70" class="d-inline-block align-top"></a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudo-menu" aria-expanded="false">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="conteudo-menu">
+			<ul class="navbar-nav mr-auto">
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" role="button" id="drop-produtos" data-toggle="dropdown"><ion-icon name="cube"></ion-icon> produtos</a>
+						<div class="dropdown-menu" aria-labelledby="drop-produtos">
+							<a href="#" class="dropdown-item"><img src="img/menu/vestuario.png" width="18px"> vestuário</a>
+							<a href="#" class="dropdown-item"><img src="img/menu/cutelaria.png" width="18px"> cutelaria</a>
+							<a href="#" class="dropdown-item"><img src="img/menu/arma.png" width="18px"> armas de fogo</a>
+							<a href="#" class="dropdown-item"><img src="img/menu/bushcraft.png" width="18px"> bushcraft</a>
+							<a href="#" class="dropdown-item"><img src="img/menu/alimentos.png" width="18px"> alimentos</a>
+						</div>
+					</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#"><ion-icon name="people"></ion-icon> quem somos</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#"><ion-icon name="call"></ion-icon> contato</a>
+				</li>
+				<div class="dropdown-divider"></div>
+			</ul>
+			<form class="form-inline text-sm-center justify-content-center">
+				<button href="#" class="btn btn-light btnmenu"><ion-icon name="log-in"></ion-icon> Login</button>
+				<button href="#" class="btn btn-light btnmenu"><ion-icon name="person-add"></ion-icon> Cadastrar-se</button>
+			</form>
+		</div>
+	</nav>
